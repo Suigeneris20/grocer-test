96f9765f11df126fd7d1463b5f07954a11e97c7c
Ifeoluwa Adebiyi <ariftodote@hotmail.com>
2022-06-27 17:28:30 +0000

    scripts.js edited online with Bitbucket

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -110,5 +110,6 @@ function calculateTotal() {
 	for (var i = 0; i < Tref.rows.length; ++i){
 		runningTotal += (+Tref.rows[i].cells[2].innerText)*(+Tref.rows[i].cells[3].innerText);
 	}
-	alert(`Current running total is ${runningTotal}`);
+	document.write("The total price is ", runningTotal );
+	//alert(`Current running total is ${runningTotal}`);
 }
